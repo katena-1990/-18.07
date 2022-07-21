0a0e96efc9c8bdc45e5db32cf87f0c0144cfb712
Katya Panko <katena-1990@mail.ru>
2022-07-21 14:31:19 +0300

    задача2

---
+++ задача2/Program.cs
@@ -0,0 +1,77 @@
+﻿/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int MinSum(int[,] matrix)
+{
+    int minSumLine = 0;
+    int sumLine = SumLineElements(matrix, 0);
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int tempSumLine = SumLineElements(matrix, i);
+        if (sumLine > tempSumLine)
+        {
+            sumLine = tempSumLine;
+            minSumLine = i;
+        }
+    }
+    Console.WriteLine($"{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+    return sumLine;
+}
+
+int SumLineElements(int[,] matrix, int i)
+{
+    int sumLine = matrix[i, 0];
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        sumLine += matrix[i, j];
+    }
+    return sumLine;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = int.Parse(Console.ReadLine());
+    return output;
+}
+
+Console.WriteLine("Введите число строк m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число столбцов n:");
+int n = int.Parse(Console.ReadLine());
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+MinSum(matrix);
+
